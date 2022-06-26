c8c949ee90cec5c628fd354e371cab6bcc4817ff
LamThinhPhat <92035772+LamThinhPhat@users.noreply.github.com>
2022-06-26 13:38:11 +0700

    Create Sign in Fragment and Sign up Fragment, Add dependency

--- app/src/main/java/com/example/wibercustomer/MainActivity.kt
+++ app/src/main/java/com/example/wibercustomer/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.wibercustomer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wibercustomer.fragments.SignInFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_main_container,SignInFragment())
+            .commit();
+
+        supportActionBar?.hide()
     }
 }
---
+++ app/src/main/java/com/example/wibercustomer/viewmodels/SignInViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.wibercustomer.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+
+    private val _phoneNumberText = MutableLiveData<String>().apply {
+        value = "+84 888"
+    }
+
+    val phoneNumberText : LiveData<String> = _phoneNumberText
+
+    private val _passwordText = MutableLiveData<String>().apply {
+        value = "ThisIsPassWord"
+    }
+
+    val passwordText : LiveData<String> = _passwordText
+
+}
