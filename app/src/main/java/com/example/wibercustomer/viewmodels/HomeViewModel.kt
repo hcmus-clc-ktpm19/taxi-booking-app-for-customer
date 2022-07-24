854cbde534473b6157cf844bf47e733afa95619c
lamthinhphat <92035772+LamThinhPhat@users.noreply.github.com>
2022-07-24 14:46:41 +0700

    Add bottom sheet and set value when an address found (#22)

--- app/src/main/java/com/example/wibercustomer/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/wibercustomer/viewmodels/HomeViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.wibercustomer.viewmodels
 
+import android.content.Context
+import android.location.Geocoder
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -12,6 +14,7 @@ import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.math.roundToInt
 
 class HomeViewModel : ViewModel() {
 
@@ -25,7 +28,29 @@ class HomeViewModel : ViewModel() {
     }
     val distanceValue: LiveData<Double> = _distanceValue
 
-    fun getDirectionAndDistance(startLocation: LatLng, destinatioLocation: LatLng) {
+    private val _moneyValue = MutableLiveData<Double>().apply {
+        value = 0.0
+    }
+    val moneyValue: LiveData<Double> = _moneyValue
+
+    private val _pickingAdressValue = MutableLiveData<String>().apply {
+        value = ""
+    }
+    val pickingAddressValue: LiveData<String> = _pickingAdressValue
+
+    private val _arrivingAdressValue = MutableLiveData<String>().apply {
+        value = ""
+    }
+    val arrivingAddressValue: LiveData<String> = _arrivingAdressValue
+
+    fun getDirectionAndDistance(startLocation: LatLng, destinatioLocation: LatLng, geocoder : Geocoder) {
+        val startAddress = geocoder.getFromLocation(startLocation.latitude, startLocation.longitude, 1)
+        val destinationAddress = geocoder.getFromLocation(destinatioLocation.latitude, destinatioLocation.longitude, 1)
+
+        val startAddressline = startAddress.get(0).getAddressLine(0)
+        val toAddressline = destinationAddress.get(0).getAddressLine(0)
+        _pickingAdressValue.value = startAddressline.substring(0, startAddressline.indexOf(','))
+        _arrivingAdressValue.value = toAddressline.substring(0, toAddressline.indexOf(','))
         RouteService.routeService.getPolyline(
             "5b3ce3597851110001cf62488405514894ed4132af5ce11377c3a573",
             "${startLocation.longitude},${startLocation.latitude}",
@@ -54,6 +79,7 @@ class HomeViewModel : ViewModel() {
                         .get("distance"))
 
                     _distanceValue.value = distanceAPI as Double
+                    _moneyValue.value = (distanceAPI / 500) * 3000 + 15000
                     val coordinates = ArrayList<LatLng>()
                     (0 until lineString.length()).forEach {
                         val iteratorCoordinate = lineString.get(it) as JSONArray
